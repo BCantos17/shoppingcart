9ac5d28c0eddfe849c7aba575f2875203acb5ae2
Osher Cohen <osher.y.cohen@gmail.com>
2017-02-15 16:19:18 -0500

    Removed jQuery.js

--- src/main/java/com/shopping/ShoppingcartApplication.java
+++ src/main/java/com/shopping/ShoppingcartApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShoppingcartApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingcartApplication.class, args);
 	}
