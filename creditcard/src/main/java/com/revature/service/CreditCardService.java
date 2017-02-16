1d67232d33bf3ae6cfbb90f94728481b3985234c
Bryan Cantos <bcantos17@gmail.com>
2017-02-16 11:54:42 -0500

    finsihed credit card service, need testing

---
+++ creditcard/src/main/java/com/revature/service/CreditCardService.java
@@ -0,0 +1,34 @@
+package com.revature.service;
+
+import com.revature.beans.CreditCard;
+import com.revature.repository.CreditCardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CreditCardService {
+    private CreditCardRepository repository;
+
+    @Autowired
+    public void setRepository(CreditCardRepository repository) {
+        this.repository = repository;
+    }
+
+    public CreditCard insert(CreditCard address){
+        return repository.insert(address);
+    }
+
+    public CreditCard save(CreditCard address){
+        return repository.save(address);
+    }
+
+    public List<CreditCard> findByCustomerId(int customerId){
+        return repository.findByCustomerId(customerId);
+    }
+
+    public void delete(Integer id){
+        repository.delete(id);
+    }
+}
