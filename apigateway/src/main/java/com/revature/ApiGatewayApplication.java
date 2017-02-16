38a1b5f5e6e6507cc39a19efb36948239ad73ba8
Bryan Cantos <bcantos17@gmail.com>
2017-02-16 16:20:12 -0500

    got eureka to work

--- apigateway/src/main/java/com/revature/ApiGatewayApplication.java
+++ apigateway/src/main/java/com/revature/ApiGatewayApplication.java
@@ -3,7 +3,6 @@ package com.revature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
