e71eb46cad937a7f7a338136d74f4526fd0864ba
Ychen192 <ychenq001@gmail.com>
2017-02-16 11:49:29 -0500

    working mvp for cart service

---
+++ cart/src/main/java/com/revature/web/CartController.java
@@ -0,0 +1,40 @@
+package com.revature.web;
+
+import com.revature.data.Cart;
+import com.revature.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+public class CartController {
+
+    private CartService service;
+
+    @Autowired
+    public void setService(CartService service) {
+        this.service = service;
+    }
+
+    @RequestMapping(value="/cart",
+                    method=RequestMethod.POST,
+                    produces= APPLICATION_JSON_VALUE)
+    public Cart save(@RequestBody Cart cart){
+        return service.save(cart);
+    }
+
+    @RequestMapping(value="/cart/{id}",
+                    method=RequestMethod.GET,
+                    produces=APPLICATION_JSON_VALUE)
+    public Cart findById(@PathVariable Integer id){
+        return service.findOne(id);
+    }
+
+    @RequestMapping(value="/cart/user/{userId}",
+                    method=RequestMethod.GET,
+                    produces=APPLICATION_JSON_VALUE)
+    public Cart findByUserId(@PathVariable Integer userId){
+        return service.findByUserId(userId);
+    }
+}
--- cart/src/main/java/com/shopping/data/Product.java
+++
@@ -1,7 +0,0 @@
-package com.shopping.data;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class Product {
-}
