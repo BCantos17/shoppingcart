8d8da40ab05a4fc976ea7c0db813a5e76dca58b4
Bryan Cantos <bcantos17@gmail.com>
2017-02-16 15:44:13 -0500

    changed annotations

--- apigateway/src/main/java/com/revature/ApiGatewayApplication.java
+++ apigateway/src/main/java/com/revature/ApiGatewayApplication.java
@@ -3,6 +3,7 @@ package com.revature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
