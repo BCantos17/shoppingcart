f1ccffcb168b16e7fcceb8628d2e5ec9ff9fa3f8
Osher Cohen <osher.y.cohen@gmail.com>
2017-02-17 18:52:42 -0500

    Added GET endpoint to CartController in apiGateway. Tested it with postman.

--- apigateway/src/main/java/com/revature/web/CartController.java
+++ apigateway/src/main/java/com/revature/web/CartController.java
@@ -4,10 +4,7 @@ import com.revature.beans.Cart;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -24,5 +21,11 @@ public class CartController {
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.put("http://localhost:8723/cart/cart/", cart);
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/cart/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<Cart> getCartById(@PathVariable int id){
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForEntity("http://localhost:8723/cart/cart/" + id, Cart.class);
     }
 }
