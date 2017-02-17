10437c260da66ee232d5dfcb6d04fe4218e435bf
Osher Cohen <osher.y.cohen@gmail.com>
2017-02-17 18:33:47 -0500

    Added http://localhost:8723/shopping/cart/ PUT and POST through the api gateway. Tested them in Postman.

--- apigateway/src/main/java/com/revature/web/CartController.java
+++ apigateway/src/main/java/com/revature/web/CartController.java
@@ -8,17 +8,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CartController {
 
-    @RequestMapping(value = "/cart", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity addItemToCart(@RequestBody Cart cart) {
-        return new ResponseEntity(HttpStatus.OK);
+    @RequestMapping(value = "/cart", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<Cart> addItemToCart(@RequestBody Cart cart) {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.postForEntity("http://localhost:8723/cart/cart/", cart, Cart.class);
     }
 
-    @RequestMapping(value="/cart", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity editItemInCart(@RequestBody Cart cart) {
+    @RequestMapping(value = "/cart", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity updateItemInCart(@RequestBody Cart cart) {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.put("http://localhost:8723/cart/cart/", cart);
         return new ResponseEntity(HttpStatus.OK);
     }
 }
--- cart/src/main/java/com/revature/web/CartController.java
+++ cart/src/main/java/com/revature/web/CartController.java
@@ -1,7 +1,6 @@
 package com.revature.web;
 
 import com.revature.data.Cart;
-import com.revature.data.Item;
 import com.revature.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,49 +17,57 @@ public class CartController {
         this.service = service;
     }
 
-    @RequestMapping(value="/cart",
-                    method=RequestMethod.POST,
-                    produces= APPLICATION_JSON_VALUE,
-                    consumes=APPLICATION_JSON_VALUE)
-    public Cart save(@RequestBody Cart cart){
+    @RequestMapping(value = "/cart",
+            method = RequestMethod.POST,
+            produces = APPLICATION_JSON_VALUE,
+            consumes = APPLICATION_JSON_VALUE)
+    public Cart save(@RequestBody Cart cart) {
         return service.save(cart);
     }
 
-    @RequestMapping(value="/cart/{id}",
-                    method=RequestMethod.GET,
-                    produces=APPLICATION_JSON_VALUE)
-    public Cart findById(@PathVariable String id){
+    @RequestMapping(value = "/cart",
+            method = RequestMethod.PUT,
+            produces = APPLICATION_JSON_VALUE,
+            consumes = APPLICATION_JSON_VALUE)
+    public Cart update(@RequestBody Cart cart) {
+        return service.save(cart);
+    }
+
+    @RequestMapping(value = "/cart/{id}",
+            method = RequestMethod.GET,
+            produces = APPLICATION_JSON_VALUE)
+    public Cart findById(@PathVariable String id) {
         return service.findOne(id);
     }
 
-    @RequestMapping(value="/cart/user/{userId}",
-                    method=RequestMethod.GET,
-                    produces=APPLICATION_JSON_VALUE)
-    public Cart findCartByUserId(@PathVariable Integer userId){
+    @RequestMapping(value = "/cart/user/{userId}",
+            method = RequestMethod.GET,
+            produces = APPLICATION_JSON_VALUE)
+    public Cart findCartByUserId(@PathVariable Integer userId) {
         return service.findCartByUserId(userId);
     }
 
-    @RequestMapping(value="/cart/deleteItem/{itemId}",
-            method=RequestMethod.POST,
-            produces= APPLICATION_JSON_VALUE,
-            consumes=APPLICATION_JSON_VALUE)
-    public Cart deleteItemFromCart(@RequestBody Cart cart, @PathVariable String itemId){
+    @RequestMapping(value = "/cart/deleteItem/{itemId}",
+            method = RequestMethod.POST,
+            produces = APPLICATION_JSON_VALUE,
+            consumes = APPLICATION_JSON_VALUE)
+    public Cart deleteItemFromCart(@RequestBody Cart cart, @PathVariable String itemId) {
         return service.deleteItemFromCart(cart, itemId);
     }
 
-    @RequestMapping(value="/cart/updateItemQuantity/{itemId}/{newQuantity}",
-            method=RequestMethod.POST,
-            produces= APPLICATION_JSON_VALUE,
-            consumes=APPLICATION_JSON_VALUE)
-    public Cart updateCartItem(@RequestBody Cart cart, @PathVariable String itemId, @PathVariable Integer newQuantity){
+    @RequestMapping(value = "/cart/updateItemQuantity/{itemId}/{newQuantity}",
+            method = RequestMethod.PUT,
+            produces = APPLICATION_JSON_VALUE,
+            consumes = APPLICATION_JSON_VALUE)
+    public Cart updateCartItem(@RequestBody Cart cart, @PathVariable String itemId, @PathVariable Integer newQuantity) {
         return service.updateItemQuantity(cart, itemId, newQuantity);
     }
 
-    @RequestMapping(value="/cart/insertItem/{productId}/{itemQuantity}",
-            method=RequestMethod.POST,
-            produces= APPLICATION_JSON_VALUE,
-            consumes=APPLICATION_JSON_VALUE)
-    public Cart insertItemToCart(@RequestBody Cart cart, @PathVariable String productId, @PathVariable Integer itemQuantity){
+    @RequestMapping(value = "/cart/insertItem/{productId}/{itemQuantity}",
+            method = RequestMethod.POST,
+            produces = APPLICATION_JSON_VALUE,
+            consumes = APPLICATION_JSON_VALUE)
+    public Cart insertItemToCart(@RequestBody Cart cart, @PathVariable String productId, @PathVariable Integer itemQuantity) {
         return service.insertItemToCart(cart, productId, itemQuantity);
     }
 }
