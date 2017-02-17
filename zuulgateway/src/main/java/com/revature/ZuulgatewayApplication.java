e8e7735af0d675c7c1949e1b758b229527d55470
Bryan Cantos <bcantos17@gmail.com>
2017-02-17 10:55:32 -0500

    made new project zuulgateway

---
+++ zuulgateway/src/main/java/com/revature/ZuulgatewayApplication.java
@@ -0,0 +1,16 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ZuulgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulgatewayApplication.class, args);
+	}
+}
