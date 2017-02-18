45519e542e8522ee347f75101bf415d5be30d42d
Osher Cohen <osher.y.cohen@gmail.com>
2017-02-18 09:25:42 -0500

    Created product bean in product service.

---
+++ product/src/main/java/com/revature/data/Product.java
@@ -0,0 +1,62 @@
+package com.revature.data;
+
+public class Product {
+    private int productId;
+    private String productName;
+    private String productImage;
+    private double price;
+    private String description;
+    private String manufacturer;
+
+    public Product() {
+        super();
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+}
