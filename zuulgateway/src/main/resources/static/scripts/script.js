3624418be551ebd045a4ee253d9306c03f7d1164
Bryan Cantos <bcantos17@gmail.com>
2017-02-19 09:27:59 -0500

    moved billing controller in javascript in a different file to keep thing cleaner

--- zuulgateway/src/main/resources/static/scripts/script.js
+++ zuulgateway/src/main/resources/static/scripts/script.js
@@ -1,6 +1,10 @@
 angular.module("MainApp",['ngRoute']);
 
+angular.module("MainApp").config(function($logProvider){
+        $logProvider.debugEnabled(true);
+});
 angular.module("MainApp").controller("MainController", function($scope){
+
 
 }).config(function ($routeProvider, $locationProvider) {
     $locationProvider.hashPrefix("");
@@ -25,36 +29,4 @@ angular.module("MainApp").controller('CartController', function ($scope) {
     $scope.price = 22;
     $scope.quantity = 10;
     $scope.subtotal = $scope.price * $scope.quantity;
-});
-
-angular.module("MainApp").controller('BillingController', function ($http, $scope) {
-    $scope.send = function() {
-        var billingAddress = {
-            customerId: $scope.billingCustomerId,
-            fullName: $scope.billingFullName,
-            line1: $scope.billingLine1,
-            line2: $scope.billingLine2,
-            city: $scope.billingCity,
-            state: $scope.billingState,
-            zipCode: $scope.billingZipCode,
-            billing: true
-        };
-
-        $http({
-            url:"localhost:8723/shopping/billing/address/insert",
-            method: "POST",
-            data: billingAddress
-        }).then(function(response) {
-            // reset form
-            $scope.billingCustomerId = "";
-            $scope.billingFullName = "";
-            $scope.billingLine1 = "";
-            $scope.billingLine2 = "";
-            $scope.billingCity = "";
-            $scope.billingState = "";
-            $scope.billingZipCode = "";
-        }, function(response) {
-            console.log("Errors in data you're sending");
-        });
-    }
 });
