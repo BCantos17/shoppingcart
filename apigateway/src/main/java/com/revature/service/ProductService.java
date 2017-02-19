594f9d0b6d862c98607542cf7ada252ed968a0f0
Osher Cohen <osher.y.cohen@gmail.com>
2017-02-19 12:33:38 -0500

    Added get all products endpoint.

--- apigateway/src/main/java/com/revature/service/ProductService.java
+++ apigateway/src/main/java/com/revature/service/ProductService.java
@@ -1,7 +1,19 @@
 package com.revature.service;
 
+import com.revature.beans.Product;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient()
+@FeignClient("product")
 public interface ProductService {
+    @RequestMapping(value = "/", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    void storeProduct(@RequestBody Product product);
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<Product> getProductById(@PathVariable int id);
 }
