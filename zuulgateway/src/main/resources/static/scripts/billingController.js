cb2a198cd914a4ffe86dd08274075309f3e45a6b
Bryan Cantos <bcantos17@gmail.com>
2017-02-19 15:49:28 -0500

    Finished billing page

--- zuulgateway/src/main/resources/static/scripts/billingController.js
+++ zuulgateway/src/main/resources/static/scripts/billingController.js
@@ -2,17 +2,31 @@ angular.module("MainApp").controller('BillingController', function ($http, $scop
 
     $log.debug('Starting Billing Controller');
 
-    // Make fake cusomter for presentation
+    // Make fake customer for presentation
     $scope.customer = {
         id: 7
     };
+
+    var addresses           = [];
+    var shippingAddresses   = [];
+    var billingAddresses    = [];
+    var cards               = [];
 
     $http({
         url: "http://localhost:8723/shopping/billing/address/byId/" +$scope.customer.id ,
         method: "GET"
     }).then(function(response) {
-        //TODO map fetch stuff
-        $log.debug(response.data)
+        addresses = response.data;
+
+        for( var i = 0; i < addresses.length; i++ ){
+            if(addresses[i].billing === false )
+                shippingAddresses.push(addresses[i]);
+            else
+                billingAddresses.push(addresses[i]);
+        }
+
+        $scope.shippingAddresses = shippingAddresses;
+        $scope.billingAddresses  = billingAddresses;
     }, function(response) {
         console.log("GET ALL: Failed to fetch addresses");
     });
@@ -21,14 +35,13 @@ angular.module("MainApp").controller('BillingController', function ($http, $scop
         url: "http://localhost:8723/shopping/billing/creditCard/byId/" +$scope.customer.id ,
         method: "GET"
     }).then(function(response) {
-        //TODO map fetch stuff
-        $log.debug(response.data)
+        cards = response.data
+        $scope.cards = cards;
     }, function(response) {
         console.log("GET ALL: Failed to fetch credit card");
     });
 
     $scope.sendShipping = function() {
-        $log.debug('in send shipping');
         var shippingAddress = {
             customerId:     $scope.customer.id,
             fullName:       $scope.shippingFullName,
@@ -58,6 +71,9 @@ angular.module("MainApp").controller('BillingController', function ($http, $scop
         });
     }
 
+    $scope.addShippingAddresses = [];
+    $scope.addBillingAddresses  = [];
+
     $scope.sendBilling = function () {
         var billingAddress = {
             customerId: $scope.customer.id,
@@ -75,7 +91,11 @@ angular.module("MainApp").controller('BillingController', function ($http, $scop
             method: "POST",
             data: billingAddress
         }).then(function(response) {
-            $log.debug(response.data);
+            // To add new address in the list dynamically
+            if(response.data.billing === false )
+                $scope.addShippingAddresses.push(response.data);
+            else
+                $scope.addBillingAddresses.push(response.data);
             // reset form
             $scope.billingFullName  = "";
             $scope.billingLine1     = "";
@@ -88,6 +108,8 @@ angular.module("MainApp").controller('BillingController', function ($http, $scop
         });
     }
 
+    $scope.addCard = [];
+    
     $scope.sendCard = function () {
         var card = {
             customerId: $scope.customer.id,
@@ -97,14 +119,13 @@ angular.module("MainApp").controller('BillingController', function ($http, $scop
             validate:   $scope.validate
         };
         
-        $log.debug(card.expDate);
-
         $http({
             url:"http://localhost:8723/shopping/billing/creditCard/insert",
             method: "POST",
             data: card
         }).then(function(response) {
-            $log.debug(response.data);
+            // To add new card in the list dynamically
+            $scope.addCard.push(response.data);
             // reset form
             $scope.fullName = "";
             $scope.number   = "";
