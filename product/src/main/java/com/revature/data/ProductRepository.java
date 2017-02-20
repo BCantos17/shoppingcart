ee1d0d1dada61785480c8d8243ce4aa6fa2034e8
Osher Cohen <osher.y.cohen@gmail.com>
2017-02-20 16:23:00 -0500

    Changed something small

--- product/src/main/java/com/revature/data/ProductRepository.java
+++ product/src/main/java/com/revature/data/ProductRepository.java
@@ -1,11 +1,9 @@
 package com.revature.data;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
-@RestResource
 public interface ProductRepository extends MongoRepository<Product, Integer> {
     @Override
     <S extends Product> S save(S Product);
